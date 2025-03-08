495bbeda49be5ba90a88555c67e2be6bb1d949ac
Worick <118030466+LeEpicScoop@users.noreply.github.com>
2025-03-08 12:24:01 -0500

    Update RobotContainer.java, removeAlgae.java. Create ButtonID.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -24,6 +24,7 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
@@ -33,6 +34,7 @@ import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.robot.commands.DriveCommands;
 import frc.robot.commands.removeAlgae;
 import frc.robot.generated.TunerConstants;
+import frc.robot.lib.ButtonID;
 import frc.robot.subsystems.coral.CoralSubsystem;
 import frc.robot.subsystems.coral.CoralSubsystem.Setpoint;
 import frc.robot.subsystems.drive.*;
@@ -57,7 +59,7 @@ public class RobotContainer {
     private SwerveDriveSimulation driveSimulation = null;
 
     // Controller
-    private final CommandXboxController controller = new CommandXboxController(0);
+    private final Joystick controller = new Joystick(0);
 
     // Dashboard inputs
     private final LoggedDashboardChooser<Command> autoChooser;
@@ -159,42 +161,9 @@ public class RobotContainer {
                 () -> -controller.getRawAxis(0),
                 () -> -controller.getRawAxis(3)));
 
-        new JoystickButton(controller.getHID(), 1).onTrue(coralSubsystem.setSetpointCommand(Setpoint.kLevel1));
-        new JoystickButton(controller.getHID(), 2).onTrue(coralSubsystem.setSetpointCommand(Setpoint.kLevel4));
-
-        // Reset gyro / odometry
-        final Runnable resetGyro = Constants.currentMode == Constants.Mode.SIM
-                ? () -> drive.setPose(
-                        driveSimulation
-                                .getSimulatedDriveTrainPose()) // reset odometry to actual robot pose during simulation
-                : () -> drive.setPose(new Pose2d(drive.getPose().getTranslation(), new Rotation2d())); // zero gyro
-        controller.start().onTrue(Commands.runOnce(resetGyro, drive).ignoringDisable(true));
-
-        // Example Coral Placement Code
-        // TODO: delete these code for your own project
-        if (Constants.currentMode == Constants.Mode.SIM) {
-            // L4 placement
-            controller.y().onTrue(Commands.runOnce(() -> SimulatedArena.getInstance()
-                    .addGamePieceProjectile(new ReefscapeCoralOnFly(
-                            driveSimulation.getSimulatedDriveTrainPose().getTranslation(),
-                            new Translation2d(0.4, 0),
-                            driveSimulation.getDriveTrainSimulatedChassisSpeedsFieldRelative(),
-                            driveSimulation.getSimulatedDriveTrainPose().getRotation(),
-                            Meters.of(2),
-                            MetersPerSecond.of(1.5),
-                            Degrees.of(-80)))));
-            // L3 placement
-            controller.b().onTrue(Commands.runOnce(() -> SimulatedArena.getInstance()
-                    .addGamePieceProjectile(new ReefscapeCoralOnFly(
-                            driveSimulation.getSimulatedDriveTrainPose().getTranslation(),
-                            new Translation2d(0.4, 0),
-                            driveSimulation.getDriveTrainSimulatedChassisSpeedsFieldRelative(),
-                            driveSimulation.getSimulatedDriveTrainPose().getRotation(),
-                            Meters.of(1.35),
-                            MetersPerSecond.of(1.5),
-                            Degrees.of(-60)))));
-        }
-    }
+        new JoystickButton(controller, ButtonID.A).onTrue(coralSubsystem.runIntakeCommand());
+        new JoystickButton(controller, ButtonID.B).onTrue(coralSubsystem.reverseIntakeCommand());
+   }
 
     /**
      * Use this to pass the autonomous command to the main {@link Robot} class.
---
+++ src/main/java/frc/robot/lib/ButtonID.java
@@ -0,0 +1,19 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.lib;
+
+/** Add your docs here. */
+public class ButtonID {
+        public static final int A = 0;
+        public static final int B = 1;
+        public static final int X = 2;
+        public static final int Y = 3;
+        public static final int LB = 4;
+        public static final int RB = 5;
+        public static final int window_thing = 6;
+        public static final int three_lines = 7;
+        public static final int LS = 8;
+        public static final int RS = 9;
+}
